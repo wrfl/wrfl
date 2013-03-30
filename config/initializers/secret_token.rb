42907bccf2825fc73f3e7059f9e3768fad96b3d5
nikolaiwarner <n@nikolaiwarner.com>
2013-03-30 16:04:24 -0400

    inital blah

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wrfl::Application.config.secret_key_base = 'ba353048686238886aff4610d31af29d607074bf00a332777eeaa217e407b2d786347e3ee8042c4cba23ea2b55bac060e529598484aab91c2200c9f451219c69'
