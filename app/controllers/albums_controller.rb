1faa9da928d1f5088b1d6b4835c797d2e5564a76
Matthew Clarke <me@matthewclarke.com>
2013-08-12 22:05:47 -0400

    Authorize
    Added some more authorization checks and .inspect in some views.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,26 +5,31 @@ class AlbumsController < ApplicationController
   # GET /albums.json
   def index
     @albums = Album.all
+    authorize! :read, @albums
   end
 
   # GET /albums/1
   # GET /albums/1.json
   def show
+    authorize! :read, @album
   end
 
   # GET /albums/new
   def new
     @album = Album.new
+    authorize! :create, @album
   end
 
   # GET /albums/1/edit
   def edit
+    authorize! :update, @album
   end
 
   # POST /albums
   # POST /albums.json
   def create
-    @album = Album.new(album_params)
+    @album = Album.new(album_params)  
+    authorize! :create, @album
 
     respond_to do |format|
       if @album.save
@@ -40,6 +45,7 @@ class AlbumsController < ApplicationController
   # PATCH/PUT /albums/1
   # PATCH/PUT /albums/1.json
   def update
+    authorize! :update, @album
     respond_to do |format|
       if @album.update(album_params)
         format.html { redirect_to @album, notice: 'Album was successfully updated.' }
@@ -54,6 +60,8 @@ class AlbumsController < ApplicationController
   # DELETE /albums/1
   # DELETE /albums/1.json
   def destroy
+    authorize! :destroy, @album
+
     @album.destroy
     respond_to do |format|
       format.html { redirect_to albums_url }
