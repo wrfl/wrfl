ee4e10796ab5656efb7ac43519ca6e019a2631e2
jbonewits <jancy.bonewits@perceptivesoftware.com>
2014-08-19 18:59:30 -0400

    Seeding database and updating readme
    Added more instructions to readme and added more songs to seeds.rb

--- README.md
+++ README.md
@@ -7,7 +7,8 @@
 ## How to Run
 
 1. `bundle`
-1. `bundle exec rake db:migrate db:seed`
-1. `rails s`
+1. when running the first time `bundle exec rake db:migrate db:seed`
+1. on subsequent runs 'bundle exec rake db:reset'
+1. `bundle exec rails s`
 1. Visit [http://localhost:3000](http://localhost:3000).
 1. Log in as `test@example.com` / `password` for a staff user, or `admin@example.com` / `password` for an admin user.
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,30 @@ white_wedding = Track.create!(name: 'White Wedding', artist_id: billy_idol_artis
 
 Play.create!([
   {user_id: staff_user.id, album_id: billy_idol_album.id,
-   track_id: white_wedding.id}
+   track_id: white_wedding.id, played_at: 1.days.ago}
 ])
+
+beach_boys = Artist.create!(name: 'Beach Boys')
+good_vibrations_album = Album.create!(name: 'Good Vibrations', label: 'Capitol')
+good_vibrations = Track.create!(name: 'Good Vibrations', artist_id: beach_boys.id)
+
+Play.create!([
+  {user_id: staff_user.id, album_id: good_vibrations_album.id,
+   track_id: good_vibrations.id, played_at: 1.weeks.ago}
+])
+
+still_cruisin = Album.create!(name: "Still Cruisin'", label: 'Capitol')
+kokomo = Track.create!(name: 'Kokomo', artist_id: beach_boys.id)
+
+Play.create!([
+  {user_id: staff_user.id, album_id: still_cruisin.id,
+   track_id: kokomo.id, played_at: 3.weeks.ago}
+])
+
+somewhere_near_japan = Track.create!(name: 'Somewhere Near Japan', artist_id: beach_boys.id)
+
+Play.create!([
+  {user_id: staff_user.id, album_id: still_cruisin.id,
+   track_id: somewhere_near_japan.id, played_at: 3.days.ago}
+])
+
