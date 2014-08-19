ad7beb5ef53b17277702abb5255bcdb9fc3f2035
jbonewits <jancy.bonewits@perceptivesoftware.com>
2014-08-19 18:01:33 -0400

    Seeding database
    Beginning seeding database (actually Sarah did this part...checking it
    in before I change things. :-) )

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-User.create!(email: 'test@example.com', password: 'password',
-             password_confirmation: 'password', first_name: 'Test',
-             last_name: 'User', role: 'staff')
+staff_user = User.create!(email: 'test@example.com', password: 'password',
+                          password_confirmation: 'password', first_name: 'Test',
+                          last_name: 'User', role: 'staff')
 
 User.create!(email: 'admin@example.com', password: 'password',
              password_confirmation: 'password', first_name: 'Admin',
              last_name: 'User', role: 'admin')
+
+billy_idol_artist = Artist.create!(name: 'Billy Idol')
+billy_idol_album = Album.create!(name: 'Billy Idol', label: 'Chrysalis')
+white_wedding = Track.create!(name: 'White Wedding', artist_id: billy_idol_artist.id)
+
+Play.create!([
+  {user_id: staff_user.id, album_id: billy_idol_album.id,
+   track_id: white_wedding.id}
+])
