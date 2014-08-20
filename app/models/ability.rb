a1e935c1ab42f7db020de15b5a8e53b4bdbd7263
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-08-19 22:24:14 -0400

    Refine abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,6 @@
 class Ability
   include CanCan::Ability
+  attr_reader :user
 
   # roles
   # - staff
@@ -8,25 +9,64 @@ class Ability
   # - directors??
 
   def initialize user
-    user ||= User.new # guest user (not logged in)
-    if user.normal?
-      can :read, :all
-    end
-    if user.admin?
-      can :manage, :all
-    end
-    if user.staff?
-      can :create, Play
-      can :update, Play, user_id: user.id
-      can :destroy, Play, user_id: user.id
+    @user = user || User.new # guest user (not logged in)
+    add_album_abilities
+    add_artist_abilities
+    add_play_abilities
+    add_track_abilities
+    add_user_abilities
+  end
 
-      can :update, User, id: user.id
+  private
 
+  def add_album_abilities
+    if @user.staff?
       can :create, Album
-      can :create, Artist
-      can :create, Track
+    end
+    if @user.admin?
+      can :manage, Album
+    end
+  end
 
-      can :read, :all
+  def add_artist_abilities
+    if @user.staff?
+      can :create, Artist
+    end
+    if @user.admin?
+      can :manage, Artist
+    end
+  end
+
+  def add_play_abilities
+    if @user.persisted?
+      can :read, Play
+    end
+    if @user.staff?
+      can :create, Play, user_id: @user.id
+      can :update, Play, user_id: @user.id
+      can :destroy, Play, user_id: @user.id
+    end
+    if @user.admin?
+      can :manage, Play
+    end
+  end
+
+  def add_track_abilities
+    if @user.staff?
+      can :create, Track
+    end
+    if @user.admin?
+      can :manage, Track
+    end
+  end
+
+  def add_user_abilities
+    can :read, User, id: @user.id
+    can :update, User, id: @user.id
+    can :destroy, User, id: @user.id
+
+    if @user.admin?
+      can :manage, User
     end
   end
 end
